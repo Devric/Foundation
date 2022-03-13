19c6b61dd9c7a233fb437d2538d93e4ac1b398ca
Billy Huang <billbox13@gmail.com>
2022-03-13 15:17:11 +1100

    BUILD add tailwind, emotion/css

--- apps/demo_ssr/src/pages/Home.jsx
+++ apps/demo_ssr/src/pages/Home.jsx
@@ -1,13 +1,18 @@
 import GlobalState, { useState } from '../GlobalState'
 
+let styles = {
+	header: `text-3xl font-bold underline`
+}
+
 export default function Home() {
 	const state = useState(GlobalState)
 
 	return (
 		<>
-			<h1 className="text-3xl font-bold underline"> Hello world! </h1>
+			<h1 className={styles.header}  css={css`color: green`}> Hello world! </h1>
 
 			{state.get()}
+
 			<button onClick={() => state.set(p => p + 1)}>Add</button>
 		</>
 	)
--- apps/demo_ssr/vite.config.ts
+++ apps/demo_ssr/vite.config.ts
@@ -5,8 +5,20 @@ const react = require('@vitejs/plugin-react')
 */
 module.exports = {
 	plugins: [
-		react()
+		react({
+			// allow emotion to compile properly
+			jsxImportSource: "@emotion/react",
+			babel: {
+				plugins: ["@emotion/babel-plugin"],
+			},
+		}),
 	],
+	// Use for emotion/react to auto inject jsx, this allows components to have css={} property
+	esbuild: {
+		jsxFactory: `jsx`,
+    	jsxInject: `import { jsx, css } from '@emotion/react'`,
+	},
+
 	build: {
 		minify: false
 	}
