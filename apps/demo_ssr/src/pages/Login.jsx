f69908f1899c75ca144fd072221411d86918cd1f
Billy Huang <billbox13@gmail.com>
2022-03-16 01:27:55 +1100

    Update sample login ui

--- apps/demo_ssr/src/pages/Login.jsx
+++ apps/demo_ssr/src/pages/Login.jsx
@@ -47,26 +47,58 @@ export default function Login() {
 	},[])
 
 	return (
-		<div css={tw`relative items-center justify-center w-full overflow-x-hidden lg:pt-40 lg:pb-40 xl:pt-40 xl:pb-64`}>
-			<div>
-				debug {JSON.stringify(formState.get())}
+		<div css={tw`min-h-screen pt-12 md:pt-20 pb-6 px-2 md:px-0`} >
+			<header css={tw`max-w-lg mx-auto`}>
+				<a href="#">
+					<h1  css={tw`text-4xl font-bold text-center`}>Login</h1>
+				</a>
+			</header>
+
+			<main css={tw`bg-white max-w-lg mx-auto p-8 md:p-12 my-10 rounded-lg shadow-2xl`}>
+				<section>
+					<h3 css={tw`font-bold text-2xl`}>Welcome to Login</h3>
+					<p css={tw`text-gray-600 pt-2`}>Sign in to your account.</p>
+				</section>
+
+				<section css={tw`mt-10`}>
+					<form css={tw`flex flex-col`} method="POST" action="#">
+						<div css={tw`mb-6 pt-3 rounded bg-gray-200`}>
+							<label css={tw`block text-gray-700 text-sm font-bold mb-2 ml-3`}>Email</label>
+				<input type="email" value={formState.email.get()} onChange={_handleFormUpdate}  name="email" css={tw`bg-gray-200 rounded w-full text-gray-700 focus:outline-none border-b-4 border-gray-300 focus:border-purple-600 transition duration-500 px-3 pb-3`} />
+						</div>
+						<div css={tw`mb-6 pt-3 rounded bg-gray-200`}>
+							<label css={tw`block text-gray-700 text-sm font-bold mb-2 ml-3`} >Password</label>
+					<input type="password"  value={formState.pass.get()} onChange={_handleFormUpdate}  name="pass" css={tw`bg-gray-200 rounded w-full text-gray-700 focus:outline-none border-b-4 border-gray-300 focus:border-purple-600 transition duration-500 px-3 pb-3`} />
+						</div>
+						<div css={tw`flex justify-end`}>
+							<a href="#" css={tw`text-sm text-purple-600 hover:text-purple-700 hover:underline mb-6`}>Forgot your password?</a>
+						</div>
+						<button onClick={_handleLogin} css={tw`bg-purple-600 hover:bg-purple-700 text-white font-bold py-2 rounded shadow-lg hover:shadow-xl transition duration-200`}>Sign In</button>
+						<div>
+							debug {JSON.stringify(formState.get())}
+						</div>
+					</form>
+				</section>
+			</main>
+
+			<div css={tw`max-w-lg mx-auto text-center mt-12 mb-6`}>
+				<p css={tw`text-white`}>Don't have an account? 
+					<a href="#" css={tw`font-bold hover:underline`}>Sign up</a>.
+				</p>
 			</div>
-			<div>
-				Email:
-				<input name="email" type="email"    value={formState.email.get()} onChange={_handleFormUpdate} css={tw`form-input px-4 py-3 rounded-md`} />
-			</div>
-			<div>
-				Pass:
-				<input name="pass" type="password"  value={formState.pass.get()} onChange={_handleFormUpdate} css={tw`form-input px-4 py-3 rounded-md`} />
-			</div>
-			<button onClick={_handleLogin}>Login</button>
+
+			<footer css={tw`max-w-lg mx-auto flex justify-center text-white`}>
+				<a href="#" css={tw`hover:underline`}>Contact</a>
+				<span css={tw`mx-3`}>•</span>
+				<a href="#" css={tw`hover:underline`}>Privacy</a>
+			</footer>
 		</div>
 	)
 
 	function _handleFormUpdate(e) {
 		// var value = e.target.name === "tags"
-		// 					? e.target.value.split(",")
-		// 					: e.target.value;
+		// 	? e.target.value.split(",")
+		// 	: e.target.value;
 
 		var {name, value} = e.target
 		formState.set( props => ({ 
